298a43aa97c036ca2e1cf79ea7c1db888e55f78a
AzerothGT <azerothgt@gmail.com>
2025-02-03 23:12:13 +0700

    Update Loader

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var preloader = document.getElementById('preloader');
+window.addEventListener('load', function() {
+    preloader.style.display = 'none';
+});
 function showsidebar(){
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "flex";
